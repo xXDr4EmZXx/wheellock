70c806ebaaa31bfc03c69d419b353cd83b200277
xXDr4EmZXx <150351868+xXDr4EmZXx@users.noreply.github.com>
2026-01-01 12:11:25 +0100

    fxmanifest version 1.0.0
    This file defines the metadata and client scripts for the GTA V mod.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'xXDr4EmZXx'
+description 'Stabilizes wheel angle and prevents unnecessary steering resets.'
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+    'client.lua'
+}
